66039114964e57402f1d1dc2cc5b51c6cbdcdf68
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-03-02 21:19:23 -0400

    feat: req 7 and req 8

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+function removeSelected() {
+  const listItems = document.getElementById('lista-tarefas').children;
+  for (let i = 0; i < listItems.length; i += 1) {
+    if (listItems[i].className) {
+      listItems[i].removeAttribute('class');
+    }
+  }
+}
+
 function createListItem(text) {
   const li = document.createElement('li');
   li.innerText = text;
   li.addEventListener('click', () => {
-    if (li.className) {
+    if (li.className === 'selected') {
       li.removeAttribute('class');
     } else {
+      removeSelected();
       li.className = 'selected';
     }
   });
